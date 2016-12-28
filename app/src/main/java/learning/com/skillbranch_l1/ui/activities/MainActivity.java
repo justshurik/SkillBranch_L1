a149904339d8dd36498d0b5170b500c6e5f6c0e8
justshurik <zaq01@ya.ru>
2016-12-28 12:26:31 +0300

    Test commit

--- app/src/main/java/learning/com/skillbranch_l1/ui/activities/MainActivity.java
+++ app/src/main/java/learning/com/skillbranch_l1/ui/activities/MainActivity.java
@@ -1,27 +1,40 @@
 package learning.com.skillbranch_l1.ui.activities;
 
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.PersistableBundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import learning.com.skillbranch_l1.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    SharedPreferences save;
-    boolean ed;
+    Button btnChangeColor;
+    TextView tvText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       if(savedInstanceState==null) Log.d("TAG","Активити создано впервые");
-       else Log.d("TAG","Активити уже было создано");
+        btnChangeColor = (Button) findViewById(R.id.btnChangeColor);
+        tvText = (TextView) findViewById(R.id.tvText);
+
+        btnChangeColor.setOnClickListener(onClickListener);
 
     }
 
-   
 
-
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            tvText.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+        }
+    };
 }
